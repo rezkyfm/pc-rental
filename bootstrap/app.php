d90de77da328f239b2d6802c2a87d1d752b95641
Rezky Firmansyah <rezky@sangkil.net>
2025-04-28 12:12:19 +0700

    fix trustedproxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 use App\Http\Middleware\EnsureUserIsCustomer;
 use App\Http\Middleware\CheckRole;
@@ -15,15 +16,21 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(EnsureUserIsCustomer::class);
-        
+
         // Register the 'role' middleware
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
 
-        $middleware->trustProxies(at: [
-            '*',
-        ]);
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
